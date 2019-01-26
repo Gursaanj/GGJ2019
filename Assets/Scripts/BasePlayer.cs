59416ff9d41d4d202eb9ff450842fce59dcf66bd
Gursaanj <gursaanjresides@gmail.com>
2019-01-26 09:52:08 -0800

    Character Movement
    Its fine

--- Assets/Scripts/BasePlayer.cs
+++ Assets/Scripts/BasePlayer.cs
@@ -11,36 +11,21 @@ public class BasePlayer : MonoBehaviour {
     public float _RangeCooldownTime = 5.0f;
     private bool _DodgeCooldown = false;
     public float _DodgeCooldownTime = 5.0f;
+    private Rigidbody2D  playerRigidbody;
 
 
     // Use this for initialization
     void Start () {
-		
+        playerRigidbody = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        Vector2 amountToMove;
-        if (Input.GetKey(KeyCode.S))
-        {
-           amountToMove = new Vector2(0, -Time.deltaTime * _speed);
-           transform.Translate(amountToMove);
-        }
-        if (Input.GetKey(KeyCode.W))
-        {
-            amountToMove = new Vector2(0, Time.deltaTime * _speed);
-            transform.Translate(amountToMove);
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            amountToMove = new Vector2(-Time.deltaTime * _speed,0);
-            transform.Translate(amountToMove);
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            amountToMove = new Vector2(Time.deltaTime * _speed, 0);
-            transform.Translate(amountToMove);
-        }
+        //Vector2 amountToMove;
+    
+        Vector3 direction = InputManager.MainInput(); //Get input
+        Move(direction);
+    
         if (Input.GetMouseButtonDown(0))
         {
             Shoot();
@@ -73,5 +58,10 @@ public class BasePlayer : MonoBehaviour {
     private void Melee()
     {
        // todo melee
+    }
+
+    private void Move(Vector3 direction)
+    {
+        playerRigidbody.velocity = (Vector3.Normalize(direction) * _speed);
     }
 }
