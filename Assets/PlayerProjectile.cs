1891e23ccb89feb6fa3c6f24212e262e0acadbd3
Martin <m4rtin.t@gmail.com>
2019-01-25 21:49:15 -0800

    projectile and object pooler

---
+++ Assets/PlayerProjectile.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerProjectile : Projectile {
+
+    string[] blockade = { "Wall", "Enemy" };
+
+    protected override void movementPerFrame()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        string tag = collision.tag;
+        if (tag == "Enemy")
+        {
+            //TODO - pass in damage
+        }
+
+        disableIfNeeded(tag);
+    }
+
+    void disableIfNeeded(string tag)
+    {
+        for (int i = 0; i < blockade.Length; i++)
+        {
+            if (tag == blockade[i])
+            {
+                gameObject.SetActive(false);
+            }
+        }
+    }
+}
---
+++ Assets/Projectile.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Projectile : MonoBehaviour {
+
+    [SerializeField]
+    float damage;
+    [SerializeField]
+    float speed;
+
+	// Update is called once per frame
+	void Update () {
+        movementPerFrame();
+    }
+
+    protected abstract void movementPerFrame();
+}
