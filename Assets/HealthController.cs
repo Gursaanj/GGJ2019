17ca199b2a97d081ef84986f03703ff11a8a04cb
Martin <m4rtin.t@gmail.com>
2019-01-27 10:11:05 -0800

    Health bar and decremeant health

---
+++ Assets/HealthController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthController : MonoBehaviour {
+
+    [SerializeField]
+    GameObject[] hearts;
+
+    [SerializeField]
+    [Range(1, 3)]
+    int lives;
+
+    int currentLives;
+
+    private void Awake()
+    {
+        currentLives = lives;
+        for (int i = 0; i < hearts.Length; i++)
+        {
+            bool isActive = i+1 <= lives;
+            hearts[i].SetActive(isActive);
+        }
+    }
+
+    public void decremeantHearts()
+    {
+        currentLives--;
+        hearts[currentLives].SetActive(false);
+    }
+}
