cadcde75f6c4a499d2c78e164d9e9767a4959a3f
Martin <m4rtin.t@gmail.com>
2019-01-27 13:23:59 -0800

    Loading level

--- Assets/Scripts/TransitionManager.cs
+++ Assets/Scripts/TransitionManager.cs
@@ -44,6 +44,6 @@ public class TransitionManager : MonoBehaviour {
     {
         //TODO - Change to increment scene index
         //.LoadScene(1);
-        SceneManager.LoadScene(1);
+        SceneManager.LoadScene(2);
     }
 }
